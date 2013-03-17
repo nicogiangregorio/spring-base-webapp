9191c18e9c27030f31978d554843b8ce02b17066
nicogorio <nicogorio@gmail.com>
2013-03-17 19:20:34 +0100

    Base spring web-app with Spring MVC

---
+++ src/main/java/it/nicogiangregorio/home/HomeController.java
@@ -0,0 +1,13 @@
+package it.nicogiangregorio.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/")
+    public String home() {
+        System.out.println("HomeController: Passing through...");
+        return "WEB-INF/views/home.jsp";
+    }
+}
